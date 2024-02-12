ddbdb819daac827564d05e99c0e4a636be8ef5cd
adryanprawira <adryanmagatsu88@gmail.com>
2024-02-12 21:47:52 +0700

    bloc add + remove

--- lib/features/cart/bloc/cart_event.dart
+++ lib/features/cart/bloc/cart_event.dart
@@ -2,3 +2,10 @@ part of 'cart_bloc.dart';
 
 @immutable
 sealed class CartEvent {}
+
+class CartInitialEvent extends CartEvent {}
+
+class CartRemoveFromCartEvent extends CartEvent {
+  CartRemoveFromCartEvent({required this.productDataModel});
+  final ProductDataModel productDataModel;
+}
